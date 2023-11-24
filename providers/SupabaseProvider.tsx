f54aef5081c58513bf5d1ac73c3128ff08fa78e9
hm-sezer <hasanmertsezers@gmail.com>
2023-11-24 11:07:09 +0300

    first commit

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useState } from "react";
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+import { Database } from "@/types/types_db";
+
+interface SupabaseProviderProps {
+  children: React.ReactNode;
+};
+
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({
+  children
+}) => {
+    const [supabaseClient] = useState(() =>
+    createPagesBrowserClient<Database>()
+  );
+
+  return ( 
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  );
+}
+ 
+export default SupabaseProvider;
