191123916c3d56b4798865a71de6469c6c6fe8ba
hm-sezer <hasanmertsezers@gmail.com>
2023-11-25 19:51:02 +0300

    fetching songs, search and debounce

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,14 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import PageContent from "./components/PageContent";
 
-export default function Home() {
+export const revalidate = 0;
+
+export default async function Home() {
+
+  const songs = await getSongs();
+
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
@@ -10,7 +17,7 @@ export default function Home() {
             Welcome back
           </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-            <ListItem image="/images/liked.png" name="Liked Songs" href="liked"/>
+            <ListItem image="/images/liked.png" name="Liked Songs" href="liked" />
           </div>
         </div>
       </Header>
@@ -20,10 +27,8 @@ export default function Home() {
             Newest songs
           </h1>
         </div>
-        <div>
-          List of Songs!
-        </div>
-      </div>  
+        <PageContent songs={songs}/>
+      </div>
     </div>
   )
 }
